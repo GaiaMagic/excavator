c8583a838c9feb8aeb89353460ee87f1ee18f085
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-11-05 13:53:19 +0800

    move routes in excavator.js into routes/index.js

--- excavator.js
+++ excavator.js
@@ -1,15 +1,9 @@
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
 var mongoose = require('mongoose');
-var livereload = require('connect-livereload');
-
+var Admin = require('./models/admin');
 var defaultAdmin = 'caiguanhao';
-
 mongoose.connect('mongodb://localhost/excavator', function () {
   console.log('mongoose is running');
 
-  var Admin = require('./models/admin');
   Admin.register(defaultAdmin, '123456').then(function () {
     console.log('created new admin: ' + defaultAdmin);
   }, function () {
@@ -17,28 +11,9 @@ mongoose.connect('mongodb://localhost/excavator', function () {
   });
 });
 
-var excavator = express();
+var excavator = require('./routes');
 
 excavator.set('port', 3000);
-excavator.use(livereload({ port: 35729 }));
-express.static.mime.define({'text/javascript': ['js']});
-excavator.use(express.static(path.join(__dirname, 'dist')));
-excavator.use(express.static(path.join(__dirname, 'views')));
-excavator.use(express.static(path.join(__dirname, 'assets')));
-excavator.use(express.static(path.join(__dirname, 'vendors')));
-
-excavator.use('/admins', require('./routes/admins'));
-excavator.use('/forms', require('./routes/forms'));
-
-excavator.use(function (req, res, next) {
-  try {
-    fs.createReadStream(path.join(__dirname, 'views', 'index.html')).pipe(res);
-  } catch (e) {
-    next();
-  }
-});
-
-excavator.use(require('./routes/errors'));
 
 excavator.listen(excavator.get('port'), function () {
   console.log('excavator is listening on port ' + excavator.get('port'));
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var livereload = require('connect-livereload');
+
+var excavator = express();
+var root = path.join(__dirname, '..');
+
+excavator.use(livereload({ port: 35729 }));
+express.static.mime.define({'text/javascript': ['js']});
+excavator.use(express.static(path.join(root, 'dist')));
+excavator.use(express.static(path.join(root, 'views')));
+excavator.use(express.static(path.join(root, 'assets')));
+excavator.use(express.static(path.join(root, 'vendors')));
+
+excavator.use('/admins', require('./admins'));
+excavator.use('/forms', require('./forms'));
+
+excavator.use(function (req, res, next) {
+  try {
+    fs.createReadStream(path.join(root, 'views', 'index.html')).pipe(res);
+  } catch (e) {
+    next();
+  }
+});
+
+excavator.use(require('./errors'));
+
+module.exports = excavator;
