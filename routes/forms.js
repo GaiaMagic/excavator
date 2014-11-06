13d98219e2a2574461c648d983a3d2a3060ea141
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-11-06 14:57:42 +0800

    add routes to list forms and show specific form

--- routes/forms.js
+++ routes/forms.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Form = require('../models/form');
 var FormRevision = require('../models/form-revision');
 var jsonParser = require('body-parser').json();
+
+router.get('/', function (req, res, next) {
+  Form.find({}).sort('-updated_at').skip(0).limit(5).populate('head').exec().
+  then(function (revisions) {
+    res.send(revisions);
+  }, next);
+});
+
+router.get('/:formid', function (req, res, next) {
+  Form.findById(req.params.formid).populate('head').exec().
+  then(function (revision) {
+    if (!revision) return next('not-found');
+    res.send(revision);
+  }, next);
+});
 
 router.post('/create', jsonParser, function (req, res, next) {
   FormRevision.create(
