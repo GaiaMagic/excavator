eae21443f00a7c0944dd4c408db3e6240f4c1357
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-11-16 22:56:14 +0800

    copy code from manager submission route to that in /control

--- routes/submissions-test.js
+++ routes/submissions-test.js
@@ -3,8 +3,10 @@ var mongoose  = require('mongoose');
 var request   = require('supertest');
 var excavator = require('./');
 var Admin     = require('../models/admin');
+var Form = require('../models/form');
 var FormRevision = require('../models/form-revision');
 var Submission = require('../models/submission');
+var Q         = require('q');
 
 var real      = config.fixturesOf('admin', 'form');
 
@@ -17,6 +19,7 @@ describe('Route /backend/submissions', function () {
   var realAdmin;
   var realForm;
   var realSubmission;
+  var realSubmission2;
 
   before(function (done) {
     if (mongoose.connection.db) {
@@ -25,21 +28,27 @@ describe('Route /backend/submissions', function () {
     mongoose.connect(config.testDBAddress, createUser(done));
 
     function createUser (done) {
-      Admin.remove({}, function () {
-        Admin.register(real.username, real.password).then(function (admin) {
-          realAdmin = admin;
-          return FormRevision.create(real.title, real.content).
-            then(function (form) {
-              realForm = form;
-            });
-        }).then(function () {
-          return Submission.submit(realForm._id, real.submit).
-            then(function (submission) {
-              realSubmission = submission;
-            }
-          );
-        }).then(done).catch(done);
-      });
+      Q.nbind(Admin.remove, Admin)({}).then(function () {
+        return Q.nbind(Form.remove, Form)({});
+      }).then(function () {
+        return Q.nbind(FormRevision.remove, FormRevision)({});
+      }).then(function () {
+        return Q.nbind(Submission.remove, Submission)({});
+      }).then(function () {
+        return Admin.register(real.username, real.password);
+      }).then(function (admin) {
+        realAdmin = admin;
+        return FormRevision.create(real.title, real.content);
+      }).then(function (form) {
+        realForm = form;
+      }).then(function () {
+        return Submission.submit(realForm._id, real.submit);
+      }).delay(100).then(function (submission) {
+        realSubmission = submission;
+        return Submission.submit(realForm._id, real.submit);
+      }).then(function (submission) {
+        realSubmission2 = submission;
+      }).catch(done).finally(done);
     }
   });
 
@@ -51,7 +60,8 @@ describe('Route /backend/submissions', function () {
       expect(200).
       end(function (err, res) {
         if (err) return done(err);
-        expect(res.body).to.be.an('array');
+        expect(res.body).to.be.an('array').and.have.length(2);
+        expect(res.body[0].created_at).to.be.above(res.body[1].created_at);
         done();
       });
     });
@@ -65,7 +75,7 @@ describe('Route /backend/submissions', function () {
       expect(404).
       end(function (err, res) {
         if (err) return done(err);
-        expect(Object.keys(res.body)).to.have.members([
+        expect(res.body).to.have.keys([
           'status',
           'type',
           'message'
@@ -76,10 +86,8 @@ describe('Route /backend/submissions', function () {
     });
 
     it('should return 404 if submissionid does not exist', function (done) {
-      var id = realSubmission._id.toString();
-      var last = ((parseInt(id.slice(-1)) || 0) + 1 + '').slice(0, 1);
       request(excavator).
-      get('/backend/submissions/' + id.slice(0, -1) + last).
+      get('/backend/submissions/' + mongoose.Types.ObjectId()).
       set('Authorization', 'token ' + realAdmin.token).
       expect(404).
       end(function (err, res) {
@@ -94,18 +102,50 @@ describe('Route /backend/submissions', function () {
       });
     });
 
-    it('should list a submission', function (done) {
+    function check (id, moreExpectations) {
+      var deferred = Q.defer();
       request(excavator).
-      get('/backend/submissions/' + realSubmission._id).
+      get('/backend/submissions/' + id).
       set('Authorization', 'token ' + realAdmin.token).
       expect(200).
       end(function (err, res) {
-        if (err) return done(err);
+        if (err) return deferred.reject(err);
+        expect(res.body).to.be.an('object').and.have.keys([
+          '__v', '_id',
+          'created_at',
+          'data',
+          'form',
+          'form_index',
+          'form_revision',
+          'form_revision_index',
+          'newer',
+          'older'
+        ]);
         expect(res.body).to.be.an('object');
         expect(res.body.form).to.be.an('object');
         expect(res.body.form_revision).to.be.an('object');
-        done();
+        if (typeof moreExpectations === 'function') moreExpectations(res.body);
+        deferred.resolve();
       });
+      return deferred.promise;
+    }
+
+    it('should list a submission', function (done) {
+      check(realSubmission._id, function (body) {
+        expect(body.older).to.be.null;
+        expect(body.newer).to.be.a('string').and.
+          equal(realSubmission2._id.toString());
+        expect(body.form_index).to.equal(0);
+        expect(body.form_revision_index).to.equal(0);
+      }).then(function () {
+        return check(realSubmission2._id, function (body) {
+          expect(body.older).to.be.a('string').and.
+            equal(realSubmission._id.toString());
+          expect(body.newer).to.be.null;
+          expect(body.form_index).to.equal(1);
+          expect(body.form_revision_index).to.equal(1);
+        });
+      }).then(done).catch(done);
     });
   });
 });
--- routes/submissions.js
+++ routes/submissions.js
@@ -1,22 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var Submission = require('../models/submission');
+var Q = require('q');
 
-router.get('/', function (req, res, next) {
-  Submission.find({}).sort('-created_at').skip(0).limit(20).
-  populate('form_revision', 'title').exec().
-  then(function (submissions) {
+function makePromise (promise) {
+  return Q.nbind(promise.exec, promise)();
+}
+
+router.get('/:submissionid([a-f0-9]{24})?', function (req, res, next) {
+  var promise;
+  var subid = req.params.submissionid;
+  if (subid) {
+    promise = makePromise(Submission.findById(subid).
+    populate('form form_revision')).then(function (submission) {
+      if (!submission) return;
+      return Q.all([
+        makePromise(Submission.find({
+          _id:  { $gt: subid },
+          form: submission.form._id
+        }).sort({ _id: 1 }).limit(1).select('_id')),
+        makePromise(Submission.find({
+          _id:  { $lt: subid },
+          form: submission.form._id
+        }).sort({ _id: -1 }).limit(1).select('_id'))
+      ]).then(function (ret) {
+        submission = submission.toObject();
+        submission.newer = ret[0][0] ? ret[0][0]._id : null;
+        submission.older = ret[1][0] ? ret[1][0]._id : null;
+        return submission;
+      });
+    });
+  } else {
+    promise = makePromise(Submission.find({}).
+      sort({ _id: -1 }).populate('form_revision'));
+  }
+  promise.then(function (submissions) {
+    if (!submissions) return next('not-found');
     res.send(submissions);
-  }, next);
-});
-
-router.get('/:submissionid', function (req, res, next) {
-  Submission.findById(req.params.submissionid).
-  populate('form form_revision').exec().
-  then(function (submission) {
-    if (!submission) return next('not-found');
-    res.send(submission);
-  }, next);
+  }).catch(next);
 });
 
 module.exports = router;
