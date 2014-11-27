d58d27ec20814b919f1ed1cebc796e4ac628fbc9
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-11-27 12:26:17 +0800

    remove fixture

--- assets/js/admin/login.js
+++ assets/js/admin/login.js
@@ -5,8 +5,6 @@ controller('controller.control.admin.login', [
   'func.location.goto',
   'func.panic',
   function (login, goto, panic) {
-    this.username = 'caiguanhao';
-    this.password = '123456';
     this.login = function () {
       login(this.username, this.password).then(function (res) {
         goto('/');
--- assets/js/manager/login.js
+++ assets/js/manager/login.js
@@ -5,8 +5,6 @@ controller('controller.manager.manager.login', [
   'func.location.goto',
   'func.panic',
   function (login, goto, panic) {
-    this.username = 'newyork';
-    this.password = '123456';
     this.login = function () {
       login(this.username, this.password).then(function (res) {
         goto('/');
