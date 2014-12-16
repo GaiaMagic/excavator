7b8086508353a4ea29d25f286784dedbe4341b7d
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-12-16 13:50:29 +0800

    local development should bind to localhost

--- excavator.js
+++ excavator.js
@@ -13,9 +13,13 @@ require('./models').connect();
 
 var excavator = require('./routes');
 excavator.set('port', 3000);
-excavator.listen(excavator.get('port'), function () {
+excavator.set('address', '0.0.0.0');
+if (environment === 'development') {
+  excavator.set('address', 'localhost');
+}
+excavator.listen(excavator.get('port'), excavator.get('address'), function () {
   console.log('Excavator is listening on port ' + excavator.get('port') +
     ' in ' + environment + ' mode.');
-  console.log('Administrator interface: http://localhost:' +
-    excavator.get('port') + '/login')
+  console.log('Administrator interface: http://' + excavator.get('address') +
+    ':' + excavator.get('port') + '/login')
 });
